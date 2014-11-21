d8312ff1b38495c0dd693e6b01a766436ceaa874
vinayakpawar <vinayakgpawar@gmail.com>
2014-11-21 11:06:13 -0500

    Assigner updated - 1. connects to server 2. sends multiple messages on individual queue

--- GroupProject/src/Assigner.java
+++ GroupProject/src/Assigner.java
@@ -1,27 +1,119 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Date;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 
 public class Assigner {
 	
+	String queueFromAssigner;
+	String queueFromServer;
+	String jobId;
+	String initialConnectionQueue;
+	String ipAddressRabbitMqServer;
+	
+	class InputtoServer {
 		
+	}
+	
+	Assigner(String ipAddress) {
+		//ip address of rabbitmq server
+		this.ipAddressRabbitMqServer = ipAddress;
+		this.initialConnectionQueue = "assignerQueue";
+		//form myQueue as, assigner + hash value of (localhost + current date time)
+		InetAddress ip;
+		Date currentDate = new Date();
+		try {
+			ip = InetAddress.getLocalHost();
+			this.queueFromAssigner = ip + currentDate.toString();
+			this.jobId = Integer.toString(this.queueFromAssigner.hashCode());
+			this.queueFromServer = "FS" + Integer.toString(this.queueFromAssigner.hashCode());
+			this.queueFromAssigner = "FA" + Integer.toString(this.queueFromAssigner.hashCode());
+
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+			
+	}
 	/**
-	 * This class contains main. 
-	 * 1. End user needs to write a separate class which implements
-	 * IAssigner interface.
-	 * 2. Create a jar file containing that class. (this may be done by
-	 * main method here - need to investigate.)
-	 * 3. Execute Assigner java program and provide
-	 * command line arguments - classname.jar data.txt
-	 * 4. main method will send this data to Server for processing.
-	 * 5. main method will receive results, error messages, logs.
-	 * 6. program will exit after it has received response from server. 
-	 * 7. Multithreading wont be required as Assigner will run as an individual instance
-	 * each time mapper and reducer functions are sent to server
-	 * 
+	 * This class contains main.
+	 * First parameter contains ip address of rabbitmq server 
 	 * @param args
 	 */	
-	
+
 	public static void main(String[] args) {
-		//validate jar file and text file
-		//send these 2 components to Server
-		//wait for responses
+		
+		Assigner myObject = new Assigner(args[0]);
+		System.out.println("connecting on "+myObject.ipAddressRabbitMqServer);
+		ConnectionFactory factory = new ConnectionFactory();
+	    factory.setHost(myObject.ipAddressRabbitMqServer);
+	    Connection connection = null;
+		
+	  //send job id to server and initialize queue names
+	    
+	    try {
+			connection = factory.newConnection();
+			Channel channel = connection.createChannel();
+
+		    channel.queueDeclare(myObject.initialConnectionQueue, false, false, false, null);
+		    channel.basicPublish("", myObject.initialConnectionQueue, null, myObject.jobId.getBytes());
+		    System.out.println(myObject.jobId);
+		    channel.close();
+		    connection.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	    
+	    
+	    Channel channel = null;
+	    try {
+			connection = factory.newConnection();
+			channel = connection.createChannel();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	    
+	    
+	    for ( int i = 0; i < 10; i++ ) {
+		    //send some text to server to test its own queue
+	    	try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			try {
+				
+				
+				String message = "Assigner message" + myObject.jobId + i;
+			    channel.queueDeclare(myObject.queueFromAssigner, false, false, false, null);
+			    channel.basicPublish("", myObject.queueFromAssigner, null, message.getBytes());
+			    System.out.println("message sent " + message);
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	    	
+	    }
+	    try {
+			channel.close();
+			connection.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	    
 	}
 }
