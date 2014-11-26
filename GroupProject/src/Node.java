83bf0fa1ce97e4f634ccbaab1a06358a31524db9
vinayakpawar <vinayakgpawar@gmail.com>
2014-11-26 14:36:16 -0500

    Draft

--- GroupProject/src/Node.java
+++ GroupProject/src/Node.java
@@ -2,5 +2,5 @@
 public class Node {
 	//implement similar communication mechanism as that of assigner
 	//implement 'node polling server' for failure handling
-	//keep placeholders for map and reduce functions
+	//keep placeholder for map and reduce functions
 }
