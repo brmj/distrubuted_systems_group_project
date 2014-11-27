7c9e2b80884cfbed88a9222a50784e7be1b6f0fb
vinayakpawar <vinayakgpawar@gmail.com>
2014-11-27 00:30:03 -0500

    Updated Assigner.java - This can be final version of assigner class, it supports following - 1. Publishes its identity (as jobid) to Server. 2. Starts communicating on it's individual queues. 3. Publishes map, reduce, and input files. 4. waits for incoming file (outputfile) which may contain either data or error message. 5. Once received, this application terminates.

--- GroupProject/src/Assigner.java
+++ GroupProject/src/Assigner.java
@@ -1,13 +1,20 @@
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Date;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ConsumerCancelledException;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.ShutdownSignalException;
 
 
 public class Assigner {
@@ -18,11 +25,16 @@ public class Assigner {
 	String initialConnectionQueue;
 	String ipAddressRabbitMqServer;
 	
+	String mapPath;
+	String reducePath;
+	String inputDataPath;
+	String outputPath;
+	
 	class InputtoServer {
 		
 	}
 	
-	Assigner(String ipAddress) {
+	Assigner(String ipAddress, String map, String reduce, String input, String output) {
 		//ip address of rabbitmq server
 		this.ipAddressRabbitMqServer = ipAddress;
 		this.initialConnectionQueue = "assignerQueue";
@@ -41,7 +53,10 @@ public class Assigner {
 			e.printStackTrace();
 		}
 			
-			
+		this.mapPath = map;
+		this.reducePath = reduce;
+		this.inputDataPath = input;
+		this.outputPath = output;
 	}
 	/**
 	 * This class contains main.
@@ -51,13 +66,16 @@ public class Assigner {
 
 	public static void main(String[] args) {
 		
-		Assigner myObject = new Assigner(args[0]);
+		Assigner myObject = new Assigner(args[0], args[1], args[2], args[3], args[4]);
 		System.out.println("connecting on "+myObject.ipAddressRabbitMqServer);
 		ConnectionFactory factory = new ConnectionFactory();
 	    factory.setHost(myObject.ipAddressRabbitMqServer);
 	    Connection connection = null;
 		
-	  //send job id to server and initialize queue names
+	    //send job id to server and initialize queue names
+	    //make initial communication with server and update jobid
+	    //so that server can start communication on individual assigner queues
+	    
 	    
 	    try {
 			connection = factory.newConnection();
@@ -73,7 +91,8 @@ public class Assigner {
 			e.printStackTrace();
 		}
 	    
-	    
+	    //initial communication has been made
+	    //assigner and server now communicate on assigner specific queue
 	    
 	    Channel channel = null;
 	    try {
@@ -83,37 +102,76 @@ public class Assigner {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-	    
-	    
-	    for ( int i = 0; i < 10; i++ ) {
-		    //send some text to server to test its own queue
-	    	try {
-				Thread.sleep(10000);
-			} catch (InterruptedException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
+	
+	    //Write map, reduce scripts and input data to server - publish on queue
+	    //and wait for output - consume from queue
+  		try {
+			
+			
+  			Path mapFilePath = Paths.get(myObject.mapPath);
+  			Path reduceFilePath = Paths.get(myObject.reducePath);
+  			Path inputFilePath = Paths.get(myObject.inputDataPath);
+  			Path outputFilePath = Paths.get(myObject.outputPath);
+  			
+		    channel.queueDeclare(myObject.queueFromAssigner, false, false, false, null);
+		    
+		    //pass map function
+		    String message = "map";
+		    channel.basicPublish("", myObject.queueFromAssigner, null, message.getBytes());
+		    channel.basicPublish("", myObject.queueFromAssigner, null, Files.readAllBytes(mapFilePath));
+		    
+		    //pass reduce function
+		    message = "reduce";
+		    channel.basicPublish("", myObject.queueFromAssigner, null, message.getBytes());
+		    channel.basicPublish("", myObject.queueFromAssigner, null, Files.readAllBytes(reduceFilePath));
+
+		    //pass input data
+		    message = "input";
+		    channel.basicPublish("", myObject.queueFromAssigner, null, message.getBytes());
+		    channel.basicPublish("", myObject.queueFromAssigner, null, Files.readAllBytes(inputFilePath));
+		    
+		    //consume from server queue
+			channel.queueDeclare(myObject.queueFromServer, false, false, false, null);
+			
+			QueueingConsumer consumer = new QueueingConsumer(channel);
+			channel.basicConsume(myObject.queueFromServer, true, consumer);
+			
 			try {
 				
+				//consume results in the form of output file
+				//outputfile will contain either output or error message if any
 				
-				String message = "Assigner message" + myObject.jobId + i;
-			    channel.queueDeclare(myObject.queueFromAssigner, false, false, false, null);
-			    channel.basicPublish("", myObject.queueFromAssigner, null, message.getBytes());
-			    System.out.println("message sent " + message);
-
-			} catch (IOException e) {
+				QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+				
+			//	Path current = Paths.get("");
+			//	String outputFileString = current.toAbsolutePath().toString() + "/output.txt";
+				
+				FileOutputStream outputFile = new FileOutputStream(myObject.outputPath);				
+				outputFile.write(delivery.getBody());
+				outputFile.close();
+				
+			} catch (ShutdownSignalException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}	    	
-	    }
-	    try {
-			channel.close();
-			connection.close();
+			} catch (ConsumerCancelledException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	    	
+
+		try {
+		channel.close();
+		connection.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    
-	    
 	}
 }
